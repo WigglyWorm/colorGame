06f8ce736758631241004e9f528656d295eaa219
Nicola Blackstock <nblackstock68@gmail.com>
2017-02-03 21:21:14 -0800

    add style

--- colorGame.js
+++ colorGame.js
@@ -1,77 +1,80 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
+var colors = [];
+var pickedColor;
 var squares = document.querySelectorAll(".square");
-var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
-var easyBtn = document.querySelector("#easyBtn");
-var hardBtn = document.querySelector("#hardBtn");
+var modeButtons = document.querySelectorAll(".mode");
 
-easyBtn.addEventListener("click", function() {
-	hardBtn.classList.remove("selected");
-	easyBtn.classList.add("selected");
-	colors = generateRandomColors(3);
-	pickedColor = pickColor();
-	colorDisplay.textContent = pickedColor;
-	for (var i = 0; i < squares.length; i++) {
-		if(colors[i]){
-			squares[i].style.background = colors[i];
-		} else {
-			squares[i].style.display = "none";
-		}
+init(); 
 
+function init(){
+	setupModeButtons();
+	setupSquares();
+	reset();
+}
+
+function setupModeButtons() {
+	for (var i = 0; i < modeButtons.length; i++) {
+		modeButtons[i].addEventListener("click", function(){
+			modeButtons[0].classList.remove("selected");
+			modeButtons[1].classList.remove("selected");
+			this.classList.add("selected");
+			// Ternary operator replaces if/else statement
+			this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
+			reset();
+		});
 	}
-});
+}
 
-hardBtn.addEventListener("click", function() {
-	hardBtn.classList.add("selected");
-	easyBtn.classList.remove("selected");
-	colors = generateRandomColors(6);
-	pickedColor = pickColor();
-	colorDisplay.textContent = pickedColor;
+function setupSquares() {
 	for (var i = 0; i < squares.length; i++) {
-			squares[i].style.background = colors[i];
-			squares[i].style.display = "block";
+// add click listeners to squares 
+		squares[i].addEventListener("click", function() {
+			// grab color of picked square
+			var clickedColor = this.style.background;
+			// compare color to pickedColor
+			if(clickedColor === pickedColor){
+				messageDisplay.textContent = "Correct!";
+				resetButton.textContent = "Play Again?"
+				changeColors(clickedColor);
+				h1.style.background = clickedColor;
+			} else {
+				this.style.background = "#232323";
+				messageDisplay.textContent = "Try Again"
+			}
+		});
 	}
-});
+}
 
-resetButton.addEventListener("click", function() {
-	// generate new colors
-	colors = generateRandomColors(6);
+function reset (){
+	colors = generateRandomColors(numSquares);
 	// pick a new random color from array
 	pickedColor = pickColor();
 	// Change colorDisplay to match picked color
 	colorDisplay.textContent = pickedColor;
+	resetButton.textContent = "New Colors"
+
+	messageDisplay.textContent = "";
 	// change colors of squares
 	for (var i = 0; i < squares.length; i++) {
-		squares[i].style.background = colors[i];
+		if (colors[i]) {
+			squares[i].style.display = "block";
+			squares[i].style.background = colors[i];
+		} else {
+			squares[i].style.display = "none";
+		}
 	}
-	h1.style.background ="#232323";
+	h1.style.background ="steelblue";
+
+}
+
+resetButton.addEventListener("click", function() {
+	reset();
 })
 
-colorDisplay.textContent = pickedColor;
-
-for (var i = 0; i < squares.length; i++) {
-	// add initial colors to squares
-	squares[i].style.background = colors[i];
-
-	// add click listeners to squares 
-	squares[i].addEventListener("click", function() {
-		// grab color of picked square
-		var clickedColor = this.style.background;
-		// compare color to pickedColor
-		if(clickedColor === pickedColor){
-			messageDisplay.textContent = "Correct!";
-			resetButton.textContent = "Play Again?"
-			changeColors(clickedColor);
-			h1.style.background = clickedColor;
-		} else {
-			this.style.background = "#232323";
-			messageDisplay.textContent = "Try Again"
-		}
-	});
-}
 
 function changeColors(color){
 			// loop through all squares
